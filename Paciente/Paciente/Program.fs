338c0e386c6959548fdc9b7099b8797cf9ee9bed
Gabriel Ureña <126486842+GabrielOsvaldoSanchez@users.noreply.github.com>
2023-10-05 18:55:29 -0600

    Add files via upload

---
+++ Paciente/Paciente/Program.fs
@@ -0,0 +1,30 @@
+﻿type Paciente =
+    | Nombre of string
+    | Apellido of string
+    | Edad of int
+    | Enfermedad of string
+     
+     
+[<EntryPoint>]
+let main argv =
+    let nombre2 = "Gabriel"
+    let nombre = Nombre("Gabri")
+    let apellido = Apellido("Ureña")
+    let edad = Edad(17)
+    let enfermedad = Enfermedad("Artritis")
+    
+    printfn "Hola %s" nombre2
+
+    match nombre with
+    | Nombre(n) -> printfn "El nombre es: %s" n
+   
+    match apellido with
+    | Apellido(a) -> printfn "El apellido es: %s" a
+    
+    match edad with
+    | Edad(e) -> printfn "La edad es: %d" e
+     
+    match enfermedad with
+    | Enfermedad(enf) -> printfn "La enfermedad es: %s" enf
+ 
+    0
